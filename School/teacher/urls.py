e1b1aa81f72bce3c9e05f0235bb542f9e178b6f7
Ehsan Software <engrokon.rok@gmail.com>
2020-08-30 10:49:07 +0600

    progress 30/08/20

--- School/teacher/urls.py
+++ School/teacher/urls.py
@@ -4,10 +4,11 @@ from . import views
 
 urlpatterns = [
     path('teachers', views.TeacherListView.as_view(), name="teachers"),
-    path('add_teacher', views.TeacherAddView.as_view(), name="add_teacher"),
+    path('add_teacher/', views.TeacherAddView.as_view(), name="add_teacher"),
     path('teacher_detail/<int:pk>', views.TeacherDetailView.as_view(), name="teacher_detail"),
-    path('edit_teacher/<int:pk>', views.TeacherUpdateView.as_view(), name="edit_teacher"),
-    path('delete_teacher/<int:pk>', views.TeacherDeleteView.as_view(), name="delete_teacher"),
+    path('edit_teacher/<int:pk>', views.TeacherEditView.as_view(), name="edit_teacher"),
+    path('update_teacher/', views.TeacherUpdateView.as_view(), name="update_teacher"),
+    path('delete_teacher/', views.TeacherDeleteView.as_view(), name="delete_teacher"),
 
 
 ]
